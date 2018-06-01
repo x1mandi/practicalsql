27e01a62364039c0c78258d7b35541d2ad42fb9c
csabimeta <csabimeta@gmail.com>
2018-06-01 05:06:40 -0400

    Added usage line to the start_db.sh script

--- start_db.sh
+++ start_db.sh
@@ -2,6 +2,11 @@
 
 #script to initialize and start Postgres
 
+#printf "Initializing DB"
+#postgresql-setup --initdb 
+#sleep 10
+#printf "db initialized, now starting the db server"
+
 if [[ $1 == 'restart' ]]
 then
     /opt/rh/rh-postgresql96/root/usr/bin/pg_ctl -D /var/opt/rh/rh-postgresql96/lib/pgsql/data -l logfile restart &
@@ -11,5 +16,6 @@ then
    /opt/rh/rh-postgresql96/root/usr/bin/pg_ctl -D /var/opt/rh/rh-postgresql96/lib/pgsql/data -l logfile start &
    exit 0
 else
-    echo "Usage: ./startdb.sh start | restart"
+  echo "Usage: ./start_db.sh start | restart"
 fi
+
